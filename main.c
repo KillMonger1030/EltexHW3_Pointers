d33e91e5a15a78b3f6db952aab356209174fa086
KillMonger1030 <valera.chelsea2018@yandex.ru>
2023-11-25 15:22:57 +0700

    Задание 4 выполнено

--- main.c
+++ main.c
@@ -1,31 +1,47 @@
 /*Задания на указатели
-1. Поменять в целом положительном числе (типа int) значение третьего
-байта на введенное пользователем число (изначальное число также
-вводится с клавиатуры) через указатель (не применяя битовые
-операции)
-
-2. В приведенном ниже коде измените только одну строку (помеченную),
-чтобы напечатать “Результат: 12.0”.
-#include <stdio.h>
-int main(void)
-{
-float x = 5.0;
-printf("x = %f, ", x);
-float y = 6.0;
-printf("y = %f\n", y);
-float *xp = &x; // TODO: отредактируйте эту строку, и только правую часть
-уравнения
-float *yp = &y;
-printf("Результат: %f\n", *xp + *yp);
-}
-
-3. Напишите программу, в которой определите массив типа int (размер
-массива 10 элементов от 1 до 10). Используйте указатель для вывода
-элементов массива на консоль (применять можно только арифметику
-указателей, а не индексы).
-
 4. Напишите программу, которая ищет введенной строке (с клавиатуры)
 введенную подстроку (с клавиатуры) и возвращает указатель на начало
 подстроки, если подстрока не найдена в указатель записывается NULL.
 В качестве срок использовать статические массивы.ю
 */
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LENGTH 100
+
+int main(){
+    char inputString[MAX_LENGTH];
+    char subString[MAX_LENGTH];
+
+    printf("Введите строку: ");
+    fgets(inputString, sizeof(inputString), stdin);
+    inputString[strcspn(inputString, "\n")] = "\0";
+
+    printf("Введите подстроку для поиска: ");
+    fgets(subString, sizeof(subString), stdin);
+    subString[strcspn(subString, "\n")] = "\0";
+
+    char* results = NULL;
+
+    for(int i = 0; i <= strlen(inputString)-strlen(subString); ++i){
+        int j;
+        for(j = 0; j < strlen(subString); ++j){
+            if(inputString[i+j] != subString[j]){
+                break;
+            }
+        }
+        if(j == strlen(subString)){
+            results = (char*)&inputString[i];
+            break;
+        }
+    }
+
+    if (results != NULL){
+        printf("Подстрока найдена. Указатель на начало подстроки: %p\n", (void*)results);
+    }else{
+        printf("Подстрока не найдена: \n");
+    }
+
+    return 0;
+}
